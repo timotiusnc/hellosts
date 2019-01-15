c33a94ff0af7418ec4570ddcbf5e3730581aea4a
timotiusnc <timotius.nc@gmail.com>
2019-01-15 18:28:19 +0700

    First blood commit From start.spring.io

---
+++ src/main/java/id/gdplabs/hellosts/HellostsApplication.java
@@ -0,0 +1,14 @@
+package id.gdplabs.hellosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellostsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/id/gdplabs/hellosts/HellostsApplicationTests.java
@@ -0,0 +1,17 @@
+package id.gdplabs.hellosts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellostsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

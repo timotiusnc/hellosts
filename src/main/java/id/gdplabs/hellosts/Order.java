75d390fb5b48ff39a0a3a68916b1b24f395ef054
timotiusnc <timotius.nc@gmail.com>
2019-01-24 13:49:13 +0700

    Fix missing ManyToMany annotation at Order class - Add custom JPA query

--- src/main/java/id/gdplabs/hellosts/Order.java
+++ src/main/java/id/gdplabs/hellosts/Order.java
@@ -50,6 +50,7 @@ public class Order implements Serializable {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity=Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addDesign(Taco design) {
--- src/main/java/id/gdplabs/hellosts/repository/OrderRepository.java
+++ src/main/java/id/gdplabs/hellosts/repository/OrderRepository.java
@@ -3,6 +3,10 @@ package id.gdplabs.hellosts.repository;
 import id.gdplabs.hellosts.Order;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+    List<Order> readOrdersByZipAndPlacedAtBetween(String zip, Date startDate, Date endDate);
 }
